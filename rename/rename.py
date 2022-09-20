8817e6e2a4a005f0d6713d320dcfb0c59d98ab61
vincentlemstra <vincentlemstra@gmail.com>
2022-09-20 15:12:55 +0200

    add rename functionality: keep old file name

--- rename/rename.py
+++ rename/rename.py
@@ -14,21 +14,30 @@ class Renamer(QObject):
     renamedFile = pyqtSignal(Path)  # returns path of renamed file
     finished = pyqtSignal()  # emitted when process is finished
 
+
     def __init__(self, files, prefix):
         super().__init__()
-        self._files = files # list of selected files
-        self._prefix = prefix # prefix to rename
+        self._files = files  # list of selected files
+        self._prefix = prefix  # prefix to rename
 
     def renameFiles(self):
         # loop selected files
         for fileNumber, file in enumerate(self._files, 1):
             # builds new filename
             newFile = file.parent.joinpath(
-                f"{self._prefix}{str(fileNumber)}{file.suffix}"
+                # option 1: use for full new file name
+                # f"{self._prefix}{str(fileNumber)}{file.suffix}"
+
+                # option 2: use to keep old file name (with number ID)
+                # f"{self._prefix}{str(fileNumber)}{file.stem}{file.suffix}"
+
+                # option 3: use to keep old file name (without ID)
+                f"{self._prefix}{file.stem}{file.suffix}"
+                
             )
             file.rename(newFile)
-            time.sleep(0.1) # Can be used to slow down application
-            self.progressed.emit(fileNumber) # update GUI
-            self.renamedFile.emit(newFile) # update GUI
+            time.sleep(0.1)  # Can be used to slow down application
+            self.progressed.emit(fileNumber)  # update GUI
+            self.renamedFile.emit(newFile)  # update GUI
         self.progressed.emit(0)  # resets the progress
-        self.finished.emit() # finished signal 
+        self.finished.emit()  # finished signal
